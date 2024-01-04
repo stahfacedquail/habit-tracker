251d165b7d2171f90778e2df34c102999a92ff6b
tami maiwashe <stahfacedquail@gmail.com>
2024-01-04 17:12:06 +0200

    Habit and Activity classes taking shape: create and fetch

---
+++ classes/activity.py
@@ -0,0 +1,35 @@
+from multimethod import multimethod
+from typing import Optional
+from db import create_activity
+import utils
+
+
+class Activity:
+    @multimethod
+    def __init__(self, habit_uuid: str, performed_at: Optional[str] = None):
+        """
+        Create a new activity in the database and then initialise a model to represent the newly-created activity.
+        :param habit_uuid: The uuid of the habit that's being performed
+        :param performed_at: The date/time at which the habit was performed, as specified by the user.  If not
+            specified, the database will add a timestamp when the record is created.
+        """
+        new_activity = create_activity(habit_uuid, performed_at)
+        self.__parse_from_db__(new_activity)
+
+    @multimethod
+    def __init__(self, activity_tuple: tuple[str, str, str]):
+        """
+        Given a record from the `activities` database table, transfer the data to this `Activity` model object
+        :param activity_tuple: A record from the `activities` database table
+        """
+        self.__parse_from_db__(activity_tuple)
+
+    def __parse_from_db__(self, activity_tuple: tuple[str, str, str]):
+        """
+        Translate the data from an activity database record to this `Activity` model object.
+        :param activity_tuple: A record from the `activities` database table
+        :return: None
+        """
+        self.uuid = activity_tuple[0]
+        self.habit = activity_tuple[1]
+        self.performed_at = utils.to_datetime(activity_tuple[2])
--- classes/habit.py
+++ classes/habit.py
@@ -1,10 +1,82 @@
-from datetime import datetime
-from db import create_habit
+from db import create_habit, get_habit
+from multimethod import multimethod
+from typing import Union, Optional
+import utils
+from classes.activity import Activity
 
 
 class Habit:
 
-    def __init__(self, title, recurrence, created_at=None):
-        self = create_habit(title, recurrence, created_at)
-        print("Model")
-        print(self)
+    @multimethod
+    def __init__(self, title: str, recurrence: str, created_at: Optional[str] = None):
+        """
+        Create a new habit in the database and then initialise a model to represent the newly-created habit.
+        :param title: The title of the habit
+        :param recurrence: How often the habit should be performed, e.g. daily
+        :param created_at: The date/time at which the habit was created, according to the user.  If not specified, the
+            database will add a timestamp when the record is created.
+        :return: None
+        """
+        created_habit = create_habit(title, recurrence, created_at)
+        self.__parse_from_db__(created_habit)
+
+    @multimethod
+    def __init__(self, uuid: str):
+        """
+        Initialise the model to represent an existing habit (from the database).
+        :param uuid: The uuid of the habit to fetch and create a model of.
+        :return: None
+        """
+        fetched_habit = get_habit(uuid)
+        self.__parse_from_db__(fetched_habit)
+
+    def __str__(self):
+        return f"""
+Title: {self.title}
+Recurs: {self.recurrence}
+Created at: {self.created_at}
+Has been performed {len(self.activities)} times
+        """
+
+    def __refresh__(self):
+        """
+        Fetch a fresh version of the habit from the database.  Invoke this whenever data outside the `habits` table is
+        changed because the model will need to be updated too.
+        :return: None
+        """
+        fetched_habit = get_habit(self.uuid)
+        self.__parse_from_db__(fetched_habit)
+
+    def perform(self, performed_at: Optional[str] = None):
+        """
+        Indicate that this habit has been performed.
+        :param performed_at: The date/time at which the habit was performed.  If not specified, the database
+            will add the timestamp at which the record was created.
+        :return: None
+        """
+        Activity(self.uuid, performed_at)
+        self.__refresh__()
+
+    def __parse_from_db__(self, db_item: dict[str, Union[tuple[str, ...], list[tuple[str, ...]]]]):
+        """
+        Transfer the data about a habit and its activities from the database to this `Habit` model object.
+        :param db_item: A dictionary object like this: {
+                habit: (..., ... ),
+                activities: [
+                    (..., ... ),
+                    (..., ... ), etc
+                ]
+            }
+        :return: None
+        """
+        db_habit = db_item["habit"]
+        db_activities = db_item["activities"]
+
+        self.uuid = db_habit[0]
+        self.title = db_habit[1]
+        self.recurrence = db_habit[2]
+        self.created_at = utils.to_datetime(db_habit[3])
+
+        self.activities = []
+        for activity in db_activities:
+            self.activities.append(Activity(activity))
