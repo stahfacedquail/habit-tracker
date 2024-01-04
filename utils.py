61222eeb929959b7a9589ca0e582da8e96014e3d
tami maiwashe <stahfacedquail@gmail.com>
2024-01-04 17:12:41 +0200

    Function to convert date string to date object

--- utils.py
+++ utils.py
@@ -1,6 +1,11 @@
 from uuid import uuid4
+from datetime import datetime
 
 
 def make_uuid():
     return str(uuid4())
+
+
+def to_datetime(datetime_str):
+    return datetime.strptime(datetime_str, "%Y-%m-%d %H:%M:%S")
 
