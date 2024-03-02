03e4258615511ab8c7a99cd5008899934d6999ee
Tami Maiwashe <stahfacedquail@gmail.com>
2024-03-02 04:50:13 +0200

    Added a TODO

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from modules import db
 from modules.cli.home import show_home_menu
 
+
+# TODO: Do we need to explicitly disconnect from database?
 if __name__ == '__main__':
     db.connect()
     setup_required = db.setup_tables()
