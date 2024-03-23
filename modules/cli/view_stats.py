258a940cf3dbf12ab95cf722cccfca82c1f20cf1
Tami Maiwashe-Mafuxwana <stahfacedquail@gmail.com>
2024-03-23 18:54:23 +0200

    Show time when showing dates like last performanced date, habit creation date

--- modules/cli/view_stats.py
+++ modules/cli/view_stats.py
@@ -242,7 +242,7 @@ def show_stats_menu(show_home_menu_fn: Callable):
 
         print(tabulate(map(
             lambda h: [
-                prettify_datetime(value) if isinstance(value, datetime)
+                prettify_datetime(value, True) if isinstance(value, datetime)
                 else value
                 for (key, value) in h.items()
             ],
